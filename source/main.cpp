d654b80cba4b7edee42d43316d096cdeb81e312a
kuran_kuran <kuran_kuran@ka2.so-net.ne.jp>
2026-01-17 01:18:51 +0900

    修正。少しまともになったかもしれないけどうまく動作しない…

--- source/main.cpp
+++ source/main.cpp
@@ -25,7 +25,8 @@
 // GPIO18: D7
 
 static const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-static const uint CONTROL_RD = 0x2;
+static const uint WAIT_RD = 0x2;
+static const uint WAIT_WR = 0x4;
 static const uint CONTROL_MASK = 0x7;
 static const uint CONTROL_READ = 0x4;
 static const uint CONTROL_WRITE = 0x2;
@@ -33,9 +34,9 @@ static const uint ADDRESS_SHIFT = 3;
 static const uint ADDRESS_MASK = 0xFF << ADDRESS_SHIFT;
 static const uint DATA_SHIFT = 11;
 static const uint DATA_MASK = 0xFF << DATA_SHIFT;
-static const uint EMM_SIZE = 0x50000;
+static const uint EMM_SIZE = 0x50000; // 320KB
 
-uint8_t __attribute__  ((aligned(sizeof(unsigned char *) * 4096))) emmData[EMM_SIZE];
+uint8_t __attribute__  ((aligned(sizeof(uint8_t *) * 4096))) emmData[EMM_SIZE];
 volatile uint32_t emmAddress = 0;
 int toggle = 1;
 
@@ -45,22 +46,25 @@ int __not_in_flash_func(ioFunc)(void)
 	uint8_t ioAddress = 0;
 	uint8_t data = 0xFF;
 	uint32_t response = 0;
+	uint control = 0;
+	bool flag = false;
 	memset(emmData, 0, EMM_SIZE);
 	while(true)
 	{
-//		allGpio = gpio_get_all();
 		allGpio = sio_hw->gpio_in;
-		uint control = allGpio & CONTROL_MASK;
+		control = allGpio & CONTROL_MASK;
+		// I/O Read
 		if(control == CONTROL_READ)
 		{
-			// I/O Read
 			ioAddress = (allGpio >> ADDRESS_SHIFT) & 0xFF;
 			switch(ioAddress)
 			{
 			case 0:
-				data = emmData[data];
+				data = emmData[emmAddress];
+				++ emmAddress;
 				response = 1;
 				break;
+				// EMM1
 			case 0xA7:
 				data = emmData[emmAddress];
 				++ emmAddress;
@@ -73,48 +77,75 @@ int __not_in_flash_func(ioFunc)(void)
 			default:
 				response = 0;
 			}
+			control = 0;
 			if(response)
 			{
 				// Set GPIO11-18 to Output
 				gpio_set_dir_masked(DATA_MASK, DATA_MASK);
 				gpio_put_masked(DATA_MASK, ((uint32_t)data << DATA_SHIFT));
+
 				// Wait while /RD is low
-				control = 0;
 				while(control == 0)
 				{
-					allGpio = sio_hw->gpio_in;
-					control = allGpio & CONTROL_RD;
-				}
+					control = sio_hw->gpio_in & WAIT_RD;
+				};
+
 				// Set GPIO11-18 to Input
 				gpio_set_dir_masked(DATA_MASK, 0x00);
 
+/*
 if(emmAddress == 0x20000 && data == 0) {
 	toggle = 1 - toggle;
 	gpio_put(LED_PIN, toggle);
 }
+*/
 
 			}
 			else
 			{
 				// Wait while /RD is low
-				control = 0;
 				while(control == 0)
 				{
-					allGpio = sio_hw->gpio_in;
-					control = allGpio & CONTROL_RD;
-				}
+					control = sio_hw->gpio_in & WAIT_RD;
+				};
 			}
+			continue;
 		}
+		// I/O Write
 		else if(control == CONTROL_WRITE)
 		{
-			// I/O Write
 			ioAddress = (allGpio >> ADDRESS_SHIFT) & 0xFF;
 			data = (allGpio >> DATA_SHIFT) & 0xFF;
 			switch(ioAddress)
 			{
 			case 0:
 				emmData[data] = data;
+				emmAddress = 0;
 				break;
+			case 1:
+				for(int i = 0; i < 256; ++ i)
+				{
+					emmData[i] = static_cast<uint8_t>(i);
+				}
+				emmAddress = 0;
+				break;
+			case 2:
+				flag = true;
+				for(int i = 0; i < 256; ++ i)
+				{
+					if(emmData[i] != static_cast<uint8_t>(i))
+					{
+						flag = false;
+						break;
+					}
+				}
+				if(flag == true)
+				{
+					toggle = 1 - toggle;
+					gpio_put(LED_PIN, toggle);
+				}
+				break;
+				// EMM1
 			case 0xA4:
 				emmAddress = (emmAddress & 0xFFFF00) | data;
 				break;
@@ -128,10 +159,12 @@ if(emmAddress == 0x20000 && data == 0) {
 			case 0xA7:
 
 
+/*
 if(emmAddress == 0x1FFFF && data == 0) {
 	toggle = 1 - toggle;
 	gpio_put(LED_PIN, toggle);
 }
+*/
 
 				emmData[emmAddress] = data;
 				++ emmAddress;
@@ -141,6 +174,13 @@ if(emmAddress == 0x1FFFF && data == 0) {
 				}
 				break;
 			}
+			// Wait while /WR is low
+			control = 0;
+			while(control == 0)
+			{
+				control = sio_hw->gpio_in & WAIT_WR;
+			};
+			continue;
 		}
 	}
 	return 0;
@@ -148,7 +188,7 @@ if(emmAddress == 0x1FFFF && data == 0) {
 
 int main()
 {
-	stdio_init_all();
+//	stdio_init_all();
 
 	// overclock 300MHz
 	vreg_set_voltage(VREG_VOLTAGE_1_20);
