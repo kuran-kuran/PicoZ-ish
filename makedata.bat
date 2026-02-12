c145d4e1205d10307bed0ba4e0e624d793dc6370
kuran_kuran <kuran_kuran@ka2.so-net.ne.jp>
2026-02-13 08:50:30 +0900

    NZ2000_SDLoaderROMデータ追加

--- makedata.bat
+++ makedata.bat
@@ -4,8 +4,13 @@ rem arm-none-eabi-objcopy -I binary -O elf32-littlearm -B arm data\1R13DIC.ROM b
 cd data
 python mkdat.py 1R13DIC.ROM
 python mkdat.py 1R13KAN.ROM
+python mkdat.py BOOT_A_LOADER.bin
 copy 1R13DIC.cpp ..\source
 copy 1R13KAN.cpp ..\source
+copy BOOT_A_LOADER.cpp ..\source
 del 1R13DIC.cpp
 del 1R13KAN.cpp
+del BOOT_A_LOADER.cpp
 cd ..
+
+pause
